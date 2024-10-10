dd8340c77fa6ad05a7fbfb5fc4385e4e6e5b129c
Karthick0529 <karthicknandha619@gmail.com>
2024-10-10 12:48:15 +0530

    updated

--- client/src/components/firebase.js
+++ client/src/components/firebase.js
@@ -1,24 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyDI-oc81k5wjJMLqEMFyoAibH3ThfXa__E",
-  authDomain: "trendy-collection-de6bc.firebaseapp.com",
-  projectId: "trendy-collection-de6bc",
-  storageBucket: "trendy-collection-de6bc.appspot.com",
-  messagingSenderId: "794031777479",
-  appId: "1:794031777479:web:b0874cd461ec6b883700e2",
-  measurementId: "G-RMZ2F9BQ48"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth=getAuth();
-export const db=getFirestore(app);
+export const auth = getAuth();
+export const db = getFirestore(app);
 export default app;
